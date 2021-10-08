01631b2cd7b625b5a9e56697941e217fbf37bfc5
Akshatshah214 <92012702+Akshatshah214@users.noreply.github.com>
2021-10-08 18:29:43 +0530

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_cors import CORS,cross_origin
 import psycopg2
 import psycopg2.extras
 import numpy as np
 from joblib import load
+import pickle
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import uuid
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error,mean_squared_error
 from datetime import datetime
+
 
 app = Flask(__name__, static_url_path = "", static_folder = "static", template_folder = "templates")
 app.secret_key = "cairocoders-ednalan"
@@ -22,35 +27,12 @@ DB_PASS = "admin"
 def home():
    return render_template('index.html')
 
-# @app.route('/')
-# def Index():
-#     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-#     s = "SELECT * FROM simple"
-#     cur.execute(s) # Execute the SQL
-#     list_users = cur.fetchall()
-#     return render_template('simple_linear.html', list_users = list_users)
-
-
-# @app.route('/simple_linear_add', methods=['GET','POST'])
-# def add_student():
-    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    # if request.method == 'POST':
-    #     fname = request.form['fname']
-    #     lname = request.form['lname']
-    #     email = request.form['email']
-        # cur.execute("INSERT INTO students (fname, lname, email) VALUES (%s,%s,%s)", (fname, lname, email))
-        # conn.commit()
-        # flash('Student Added successfully')
-        # return redirect(url_for('Index'))
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 @app.route('/simple_linear', methods=['GET', 'POST'])
 def simple_linear():
-    # print("Simple_Linear")
+    
     try:
-        # print("First")
-        
-        # print("second")
         request_type_str = request.method
         
         try:
@@ -58,24 +40,13 @@ def simple_linear():
         except:
             return "Second"
         
-        # cur.execute("SELECT image FROM simple ORDER BY datetime DESC")
-        # image_link = cur.fetchone()
-        
-        # image_name = "static/" + image_link[0] + ".svg"
-        
-        # print(image_name)
-
         if request_type_str == 'GET':
-            # static/37856fd1878145a084368d92a75424c7.svg
-            # print("2131")
             return render_template('sLinear.html')
         else:
-            # print("Third")
             text = request.form['text']
 
             Dtime = datetime.now()
 
-            # 6a0dcc9c03574ae0b563ea938316d37f
             random_string = uuid.uuid4().hex
             path = "static/" + random_string + ".svg"
             model = load('model.joblib')
@@ -90,11 +61,6 @@ def simple_linear():
                 cur.close()
             except:
                 return "Forth"
-
-            # print("Forth")
-            # flash('Data Added Successfully!!!')
-            # return redirect(url_for('Index'))
-            # print("Fifth")            
 
             if text:
                 np_arr = floats_string_to_np_arr(text) 
@@ -113,31 +79,23 @@ def simple_linear():
 
 @app.route('/simple_linear_layout', methods=['GET', 'POST'])
 def simple_linear_layout():
-    # print("Simple_Linear")
     try:
-        
-        # print("second")
         request_type_str = request.method
         
         try:
             cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         except:
             return "Second"
-
-        # print("First")
-            
+        
         s = "SELECT * FROM simple WHERE is_delete=FALSE"
         cur.execute(s) # Execute the SQL
         list_users = cur.fetchall()
         cur.close()
-        # print("2131")
-        # print(list_users)
+
         return render_template('sLinear_layout.html', list_users = list_users)
 
     except:
         return "Connection Fail"
-
-
 
 @app.route('/simple_Linear_delete/<string:id>', methods = ['POST','GET'])
 def simple_Linear_delete(id):
@@ -146,19 +104,18 @@ def simple_Linear_delete(id):
     cur.execute('UPDATE simple SET is_delete = TRUE WHERE simpleid = {0}'.format(id))
     conn.commit()
     cur.close()
-    flash('Data Removed Successfully')
+    
     return redirect(url_for('simple_linear_layout'))
-
 
 @app.route('/simple_Linear_edit/<string:id>', methods = ['POST', 'GET'])
 def simple_Linear_edit(id):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    print(id)
-    # cur.execute('SELECT * FROM simple WHERE simpleid = %s', (id))
+    
     cur.execute('SELECT * FROM simple WHERE simpleid = %s', (id,))
+    
     data = cur.fetchone()
     cur.close()
-    print(data)
+    
     return render_template('sLinear_edit.html', sLinear = data)
 
 
@@ -174,14 +131,10 @@ def simple_Linear_update(id):
         except:
             return "Second"
 
-        
-        if request_type_str == 'POST':
-            # print("Third")
+        if request_type_str == 'POST':            
             text = request.form['txt_numbers']
 
             Dtime = datetime.now()
-
-            # 6a0dcc9c03574ae0b563ea938316d37f
 
             random_string = uuid.uuid4().hex
             path = "static/" + random_string + ".svg"
@@ -198,11 +151,6 @@ def simple_Linear_update(id):
             except:
                 return "Forth"
 
-            # print("Forth")
-            # flash('Data Added Successfully!!!')
-            # return redirect(url_for('Index'))
-            # print("Fifth")            
-
             if text:
                 np_arr = floats_string_to_np_arr(text) 
             else:
@@ -213,7 +161,6 @@ def simple_Linear_update(id):
                 return redirect(url_for('simple_linear_layout'))
             else:
                 return "Please enter Valid values => " + text
-
 
 def make_picture(training_data_filename, model, new_inp_np_arr, output_file):
     data = pd.read_pickle(training_data_filename)
@@ -246,3 +193,107 @@ def floats_string_to_np_arr(floats_str):
             return False
     floats = np.array([float(x) for x in floats_str.split(',') if is_float(x)])
     return floats.reshape(len(floats), 1)
+
+
+@app.route('/multi_linear', methods=['GET', 'POST'])
+@cross_origin()
+def multi_linear():
+    # print("Simple_Linear")
+    try:
+        print("First")
+
+        model=pickle.load(open('mLinear.pkl','rb'))
+
+        print("Second")
+
+        car=pd.read_csv('Cleaned_Car_data.csv')
+
+        print("Third")
+        
+        request_type_str = request.method
+        
+        try:
+            print("forth")
+            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        except:
+            return "Second"
+
+        print("Fifth")
+
+        if request_type_str == 'GET':
+            companies = sorted(car['company'].unique())
+            car_models = sorted(car['name'].unique())
+            year = sorted(car['year'].unique(),reverse=True)
+            fuel_type = car['fuel_type'].unique()
+            # print(companies)
+            # print(car_models)
+            # print(year)
+            # print(fuel_type)
+            companies.insert(0,'Select Company')
+            print("Sixth")
+            return render_template('mLinear.html',companies = companies, car_models = car_models, years = year,fuel_types = fuel_type)
+        else:
+            # sepal_length = request.form['sepal_length']
+            # sepal_width = request.form['sepal_width']
+            # petal_length = request.form['petal_length']
+            # petal_width = request.form['petal_width']
+            # mae  = request.form['mae']
+            # mse = request.form['mse']
+            # rmse = request.form['rmse']
+            print("First")
+            company_name=request.form.get('company')
+
+            car_model=request.form.get('car_models')
+            purchase_year=request.form.get('year')
+            fuel_type=request.form.get('fuel_type')
+            driven=request.form.get('kilo_driven')
+            print("two")
+            
+            prediction = model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
+                                    data=np.array([car_model,company_name,purchase_year,driven,fuel_type]).reshape(1, 5)))
+            
+            print(prediction)
+
+            
+
+            Dtime = datetime.now()
+
+            # 6a0dcc9c03574ae0b563ea938316d37f
+            random_string = uuid.uuid4().hex
+            path = "static/" + random_string + ".svg"
+            # model = load('mLinear.joblib')
+
+            # CREATE TABLE multi (
+            #     MultiID serial primary key NOT NULL,
+            #     company_name text not null ,
+            #     car_models text not null ,
+            #     purchase_year int not null ,
+            #     fuel_type text not null ,
+            #     kilo_driven float not null,
+            #     predict_price float not null,
+            #     datetime text not null,
+            #     image text,
+            #     is_delete bool
+            # );
+
+            try:
+                cur.execute("INSERT INTO multi(company_name,car_models,purchase_year,fuel_type,kilo_driven,predict_price,datetime,image,is_delete)VALUES (%s,%s,%d,%s,%f,%f,%s,%s,FALSE)", (company_name,car_model,purchase_year,fuel_type,driven,prediction,Dtime,random_string))
+            except:
+                return "Third"
+
+            try:
+                conn.commit()
+                cur.close()
+            except:
+                return "Forth"
+
+            return str(np.round(prediction[0],2))
+
+            # if len(np_arr):
+            #     make_picture('AgesAndHeights.pkl', model, np_arr, path)
+            #     return render_template('mLinear.html', href=path)
+            # else:
+            #     return "Please enter Valid values => " + text
+
+    except:
+        return "Connection Fail"
